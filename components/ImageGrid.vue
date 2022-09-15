99be937b14feca06d023950a16c77a8c177baf55
Mads <madsbrodt3@gmail.com>
2022-09-15 15:23:20 +0200

    Create example ImageGrid component

---
+++ components/ImageGrid.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div v-for="image in images" :key="image.id" class="w-full relative">
+      <img
+        class="rounded-md w-full cursor-pointer object-cover aspect-square"
+        :src="image.img"
+        alt=""
+        @click="imageClick(image)"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    images: {
+      type: Array,
+      default: () => [],
+    },
+  },
+};
+</script>
+
+<style scoped></style>
